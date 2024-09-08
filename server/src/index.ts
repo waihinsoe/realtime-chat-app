c030b697fecc7f605080946edacffb33a1bbb61b
waihinsoe <waihinsoe332004@gmail.com>
2024-09-08 22:49:37 +0630

    feat: setup websocket in both frontend and backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,37 @@
 import express, { Express, Request, Response } from "express";
-
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 const app: Express = express();
 const port: number = 3000;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
-  console.log("hehe");
   res.send("Hello world with typescript");
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
